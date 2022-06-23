6805e5ffbc25b9396e3a6356961d94e98b42e40b
Karine Haubert <kahaubert201270@gmail.com>
2022-06-23 15:13:10 -0300

    0.0.4
    Configurado o movimento do cartão e os pontos
    - Ficou para finalizar:
      Ajustar os colisores do banqueiro, da senhora e do cartão
      Refinar o código

--- funcoes.py
+++ funcoes.py
@@ -8,3 +8,35 @@ def cadastraJogador(nome, email):
 def continuar():
     os.system("pause")
 
+def checa_cartao(playerX, playerY, larguraPlayer, alturaPlayer, cartaoX, cartaoY, tamanhoCartao):
+    if(playerX - cartaoX)<= tamanhoCartao and (cartaoX - playerX) <= larguraPlayer:
+        if(playerY - cartaoY)<= tamanhoCartao and (cartaoY - playerY) <= alturaPlayer:
+            return True
+    return False
+
+
+def dead(gameDisplay, bg_perdeu, pygame, cor, pygameDisplay, pontos):
+    gameDisplay.blit(bg_perdeu, (0, 0))
+    # pygame.mixer.music.stop()
+    # pygame.mixer.Sound.play(explosaoSound)
+    fonte = pygame.font.Font("freesansbold.ttf", 50)
+    texto = fonte.render("Você Perdeu! Só recolheu "+str(pontos) +
+                        " cartoes :(", True, cor)
+    gameDisplay.blit(texto, (50, 100))
+    fonteContinue = pygame.font.Font("freesansbold.ttf", 25)
+    textoContinue = fonteContinue.render("press enter to restart", True, cor)
+    gameDisplay.blit(textoContinue, (50, 200))
+    pygameDisplay.update()
+
+def venceu(gameDisplay, bg_venceu, pygame, cor, pygameDisplay, pontos):
+    gameDisplay.blit(bg_venceu, (0, 0))
+    # pygame.mixer.music.stop()
+    # pygame.mixer.Sound.play(explosaoSound)
+    fonte = pygame.font.Font("freesansbold.ttf", 50)
+    texto = fonte.render("Você Ganhou! Recolheu todos os \n 10 cartões e ajudou o prof Marcão a comprar seu Civic!", True, cor)
+    gameDisplay.blit(texto, (50, 100))
+    fonteContinue = pygame.font.Font("freesansbold.ttf", 25)
+    textoContinue = fonteContinue.render("press enter to restart", True, cor)
+    gameDisplay.blit(textoContinue, (50, 200))
+    pygameDisplay.update()
+
--- game.py
+++ game.py
@@ -1,21 +1,21 @@
-from funcoes import cadastraJogador, continuar
+from funcoes import cadastraJogador, continuar, checa_cartao, dead, venceu
 import pygame
 import re
 import random
 
-print("")
+
 nomeJogador = input("Digite seu nome: ");
 emailJogador= input("Digite seu e-mail: ");
 
 while not re.match(r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$", emailJogador):
-    print("E-mail Inválido!")
-    emailJogador= input("Digite seu e-mail: ");
-    continuar();
+     print("E-mail Inválido!")
+     emailJogador= input("Digite seu e-mail: ");
+     continuar();
 
 while len(nomeJogador) < 3:
-    print("Nome Inválido!");
-    nomeJogador = input("Digite seu nome: ");
-    continuar();
+     print("Nome Inválido!");
+     nomeJogador = input("Digite seu nome: ");
+     continuar();
 
 cadastraJogador(nomeJogador, emailJogador);
 
@@ -26,8 +26,9 @@ largura = 900
 tamanho = (largura, altura)
 pygameDisplay = pygame.display
 pygameDisplay.set_caption("")
-bg = pygame.image.load("assets/fundo.png")
-bg_perdeu = pygame.image.load("assets/ganhou.png")
+bg = pygame.image.load("assets/fundo.jpeg")
+bg_perdeu = pygame.image.load("assets/perdeu.png")
+bg_ganhou = pygame.image.load("assets/ganhou.png")
 gameDisplay = pygame.display.set_mode(tamanho)
 gameEvents = pygame.event
 clock = pygame.time.Clock()
@@ -36,50 +37,36 @@ black = (0, 0, 0);
 white = (255,255,255);
 
 
-def dead(pontos):
-    gameDisplay.blit(bg_perdeu, (0, 0))
-    pygame.mixer.music.stop()
-    # pygame.mixer.Sound.play(explosaoSound)
-    fonte = pygame.font.Font("freesansbold.ttf", 50)
-    texto = fonte.render("Você Perdeu com "+str(pontos) +
-                        " pontos!", True, black)
-    gameDisplay.blit(texto, (50, 100))
-    fonteContinue = pygame.font.Font("freesansbold.ttf", 25)
-    # textoContinue = fonteContinue.render("press enter to restart", True, white)
-    # gameDisplay.blit(textoContinue, (50, 200))
-    pygameDisplay.update()
 
 def jogo():
     jogando = True
-    movimentoXvovozinha = 0
-    movimentoYvovozinha = random.randrange(0, altura)
-    movimentoXbanqueiro = 0
-    movimentoYbanqueiro = random.randrange(0, altura)
-    velocidade = 1
-    direcao = True
+
+    vovozinha = pygame.image.load("assets/senhora.png")
+    banqueiro = pygame.image.load("assets/cearensebanqueiro.png")
+    player = pygame.image.load("assets/cabecaPedro.png")
+    cartao = pygame.image.load("assets/cartao.png")
+    velocidade = 5
+   
+    movimentoXvovozinha, movimentoXbanqueiro = 0, 0
+    movimentoYvovozinha, movimentoYbanqueiro = random.randrange(0, altura), random.randrange(0, altura)
+    cartaoX, cartaoY = 0, random.randrange(0.2*largura, 0.8*altura, velocidade)
+
     posicaoXPlayer = 450
     posicaoYPlayer = 100
     movimentoXPlayer = 0
     movimentoYPlayer = 0
-    pontos = 0
-    vovozinha = pygame.image.load("assets/senhora.png")
-    vovozinha = pygame.transform.flip(vovozinha, True, False)
-    banqueiro = pygame.image.load("assets/cearensebanqueiro.png")
-    banqueiro = pygame.transform.flip(banqueiro, True, False)
-    #missile = pygame.transform.scale(missile, (300, 30))
-    player = pygame.image.load("assets/cabecaPedro.png")
-    # pygame.mixer.music.load("assets/trilha.mp3")
-    # pygame.mixer.music.play(-1)
-    # pygame.mixer.music.set_volume(0.1)
-    # pygame.mixer.Sound.play(missileSound)
-    larguraPlayer = 60
-    alturaPlayer = 60
-    larguraVovozinha = 60
-    alturaVovozinha = 60
-    largurabanqueiro = 60
-    alturabanqueiro = 60
+    
+  
+    larguraPlayer = 109
+    alturaPlayer = 149
+    larguraVovozinha = 119
+    alturaVovozinha = 130
+    largurabanqueiro = 117
+    alturabanqueiro = 130
     velocidadePlayer = 10
-    limiar = 28
+    larguraCartao = 133
+    limiar = 40
+    pontos = 0
 
     while True:
        
@@ -98,10 +85,12 @@ def jogo():
                     movimentoYPlayer = velocidadePlayer
                 elif event.key == pygame.K_RETURN:
                     jogo()
+
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                     movimentoXPlayer = 0
                     movimentoYPlayer = 0
+       
         if jogando == True:
           
             posicaoXPlayer = posicaoXPlayer + movimentoXPlayer
@@ -109,47 +98,40 @@ def jogo():
                 posicaoXPlayer = 0
             elif posicaoXPlayer > largura - larguraPlayer:
                 posicaoXPlayer = largura - larguraPlayer
+
             posicaoYPlayer = posicaoYPlayer + movimentoYPlayer
             if posicaoYPlayer < 0:
                 posicaoYPlayer = 0
             elif posicaoYPlayer > altura - alturaPlayer:
                 posicaoYPlayer = altura - alturaPlayer
+
             gameDisplay.fill(pink)
             gameDisplay.blit(bg, (0, 0))
             gameDisplay.blit(player, (posicaoXPlayer, posicaoYPlayer))
             gameDisplay.blit(vovozinha, (movimentoXvovozinha, movimentoYvovozinha))
-            gameDisplay.blit(banqueiro, (movimentoXbanqueiro, movimentoXbanqueiro))
+            gameDisplay.blit(banqueiro, (movimentoXbanqueiro, movimentoYbanqueiro))
+            gameDisplay.blit(cartao, (cartaoX, cartaoY));
             
-            if direcao == True:
-                if movimentoXvovozinha <= 900 - 150 and movimentoXbanqueiro <= 900 - 150:
-                    movimentoXvovozinha = movimentoXvovozinha + velocidade
-                    movimentoXbanqueiro = movimentoXbanqueiro + velocidade
-                else:
-                    # pygame.mixer.Sound.play(missileSound)
-                    direcao = False
-                    pontos = pontos + 1
-                    movimentoYvovozinha = random.randrange(0, altura)
-                    movimentoYbanqueiro = random.randrange(0, altura)
-                    velocidade = velocidade + 1
-                    vovozinha = pygame.transform.flip(vovozinha, True, False)
-                    banqueiro = pygame.transform.flip(banqueiro, True, False)
-            else:
-                if movimentoXvovozinha  >= 0 and movimentoYbanqueiro >= 0:
-                    movimentoXvovozinha = movimentoXvovozinha + velocidade
-                    movimentoXbanqueiro = movimentoXbanqueiro + velocidade
-                else:
-                    # pygame.mixer.Sound.play(missileSound)
-                    direcao = True
-                    pontos += 1
-                    movimentoYvovozinha = random.randrange(0, altura)
-                    movimentoYbanqueiro = random.randrange(0, altura)
-                    velocidade = velocidade + 1
-                    vovozinha = pygame.transform.flip(vovozinha, True, False)
-                    banqueiro = pygame.transform.flip(banqueiro, True, False)
-            
+            cartaoX += velocidade
+            movimentoXvovozinha += velocidade
+            movimentoXbanqueiro += velocidade
+
+            if (checa_cartao(movimentoXPlayer, movimentoYPlayer, larguraPlayer, alturaPlayer, cartaoX, cartaoY, larguraCartao)):
+                cartaoX, cartaoY = 0, random.randrange(0, altura) 
+                pontos +=1
+                print(pontos)
+             
+            if cartaoX >= largura:
+                cartaoX, cartaoY = 0, random.randrange(0, altura)   
+            if movimentoXvovozinha >= largura:
+                movimentoXvovozinha, movimentoYvovozinha = 0, random.randrange(0, altura)   
+            if movimentoXbanqueiro >= largura:
+                movimentoXbanqueiro, movimentoYbanqueiro = 0, random.randrange(0, altura)  
+
+
             
             fonte = pygame.font.Font('freesansbold.ttf', 20)
-            texto = fonte.render("Pontos: "+str(pontos), True, white)
+            texto = fonte.render("cartões: "+str(pontos), True, white)
             gameDisplay.blit(texto, (20, 280))
             
             pixelYVovozinha = list(range(movimentoYvovozinha, movimentoYvovozinha+alturaVovozinha + 1))
@@ -158,13 +140,15 @@ def jogo():
             pixelXbanqueiro= list(range(movimentoXbanqueiro, movimentoXbanqueiro+largurabanqueiro + 1))
             pixelYPlayer = list(range(posicaoYPlayer, posicaoYPlayer + alturaPlayer +1 ))
             pixelXPlayer = list(range(posicaoXPlayer, posicaoXPlayer + larguraPlayer +1 ))
-            colisaoY = list( set(pixelYVovozinha) & set(pixelYbanqueiro) & set(pixelYPlayer)  )
-            colisaoX = list( set(pixelXVovozinha) & set(pixelXbanqueiro)  & set(pixelXPlayer)  )
-            if len(colisaoY) > limiar:
-                if len(colisaoX) > limiar:
+            colisaoYPerde = list( set(pixelYVovozinha) & set(pixelYbanqueiro) & set(pixelYPlayer)  )
+            colisaoXPerde = list( set(pixelXVovozinha) & set(pixelXbanqueiro)  & set(pixelXPlayer)  )
+            if len(colisaoYPerde) > limiar:
+                if len(colisaoXPerde) > limiar:
                     jogando =  False
-                    dead(pontos)
-        
+                    dead(gameDisplay, bg_perdeu, pygame, black, pygameDisplay, pontos)
+            elif pontos == 10:
+                jogando = False
+                venceu(gameDisplay, bg_ganhou, pygame, black, pygameDisplay, pontos)
         pygameDisplay.update()
         clock.tick(60)
 jogo()
